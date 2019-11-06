ef722c7f149cc74458b405713a584dd4e2f794cd
WaterBlade <zengjiangtao_yt@126.com>
2019-11-06 11:46:50 +0800

    Export math builder

--- src/builder/index.ts
+++ src/builder/index.ts
@@ -1,3 +1,6 @@
 export {DocXBuilder} from "./docXBuilder";
 export {Reference} from "./reference";
+export {MathDeclarationBuilder} from "./mathDeclarationBuilder";
+export {MathDefinitionBuilder} from "./mathDefinitionBuilder";
+export {MathProcedureBuilder} from "./mathProcedureBuilder";
 export * from "./math";
